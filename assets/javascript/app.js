c48bdb41b84fbf559b1eb9df56ff0e558954faaf
Binh Lam <cindybinh444@gmail.com>
2018-10-01 18:19:41 -0400

    add pseudo-codes

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDehVTK8bKydeja68w3D1UaMgoMtJJ73Ck",
+    authDomain: "test-5e833.firebaseapp.com",
+    databaseURL: "https://test-5e833.firebaseio.com",
+    projectId: "test-5e833",
+    storageBucket: "test-5e833.appspot.com",
+    messagingSenderId: "635088554189"
+  };
+  firebase.initializeApp(config);
+
+
+  //Once player 1 is clicked, SHOW input option for name then click submit
+  //HIDE: the player 1 
+  //the name data is stored in firebase database
+  //HIDE: the player 1 input option and submit button 
+  //SHOW: the name info is displayed back to the page from the database 
+  //SHOW: rock, paper, and scissors buttons
+  //Once one of the option is clicked, the option is saved to the database 
+  //HIDE: all three buttons
+  //Use JQuery Show and hide() for displaying purposes 
+  
+  //Results 
+  //compare player 1 and player 2 options 
+  //pull data from the database
+  //if else statement logic for win and lose 
+  //display win or lose 
+
+
+  //Player 2 follow the above instructions!!!
+
+  //Chat Box 
+  //same thing for message
+
+
+
+  
+  
